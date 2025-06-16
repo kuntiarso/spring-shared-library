1ed9b1f303c721bc0fe7eb5421b64867f2f8d2c8
Kuntiarso <gilang.grk17@gmail.com>
2025-06-16 16:02:41 +0700

    feat(shared-library): added auditable for core pojo class

--- src/main/java/com/developer/superuser/shared/audit/StandardAuditable.java
+++ src/main/java/com/developer/superuser/shared/audit/StandardAuditable.java
@@ -1,50 +1,23 @@
 package com.developer.superuser.shared.audit;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Version;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.*;
 
 import java.time.Instant;
 
 @Getter
 @Setter
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(toBuilder = true)
+@EqualsAndHashCode
+@ToString
 public class StandardAuditable {
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
-
-    @CreatedBy
-    @Column(name = "created_by", nullable = false, updatable = false, length = 100)
     private String createdBy;
-
-    @LastModifiedDate
-    @Column(name = "updated_at", nullable = false)
     private Instant updatedAt;
-
-    @LastModifiedBy
-    @Column(name = "updated_by", nullable = false, length = 100)
     private String updatedBy;
-
-    @Column(name = "deleted", nullable = false)
     private boolean deleted = false;
-
-    @Column(name = "deleted_at")
     private Instant deletedAt;
-
-    @Column(name = "deleted_by", length = 100)
     private String deletedBy;
-
-    @Version
-    @Column(name = "version", nullable = false)
     private Long version;
 }
