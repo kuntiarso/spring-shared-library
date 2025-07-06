951bfd0790d5c4bc4599663f9099693223b9bfc5
Kuntiarso <gilang.grk17@gmail.com>
2025-07-07 02:27:58 +0700

    feat(shared-library): updated auto config imports

--- src/main/java/com/developer/superuser/shared/config/GlobalConfig.java
+++ src/main/java/com/developer/superuser/shared/config/GlobalConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.developer.superuser.shared")
+@ComponentScan(basePackages = "com.developer.superuser")
 @EnableConfigurationProperties(SharedConfigProperties.class)
 public class GlobalConfig {
 }
