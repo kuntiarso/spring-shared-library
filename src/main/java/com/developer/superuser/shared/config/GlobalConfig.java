000cb8e8db4b1f655fc32bca45b074aef82b9650
Kuntiarso <gilang.grk17@gmail.com>
2025-07-07 02:02:39 +0700

    feat(shared-library): added component scan

--- src/main/java/com/developer/superuser/shared/config/GlobalConfig.java
+++ src/main/java/com/developer/superuser/shared/config/GlobalConfig.java
@@ -2,9 +2,11 @@ package com.developer.superuser.shared.config;
 
 import com.developer.superuser.shared.property.SharedConfigProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.developer.superuser.shared")
 @EnableConfigurationProperties(SharedConfigProperties.class)
 public class GlobalConfig {
 }
