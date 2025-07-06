e8873418f69e60b148381e53fbed130d92a5ceb6
Kuntiarso <gilang.grk17@gmail.com>
2025-07-07 00:59:00 +0700

    feat(shared-library): fix component scan

--- src/main/java/com/developer/superuser/shared/SharedLibraryApplication.java
+++ src/main/java/com/developer/superuser/shared/SharedLibraryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SharedLibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SharedLibraryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SharedLibraryApplication.class, args);
+    }
 
 }
