4340104a2f0d930d592f9e2fc22316d9e8a1bce9
Kuntiarso <gilang.grk17@gmail.com>
2025-07-20 19:16:49 +0700

    feat(shared-library): updated auditable class

--- src/main/java/com/developer/superuser/shared/audit/StandardAuditable.java
+++ src/main/java/com/developer/superuser/shared/audit/StandardAuditable.java
@@ -1,5 +1,6 @@
 package com.developer.superuser.shared.audit;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -13,12 +14,20 @@ import java.time.Instant;
 @EqualsAndHashCode
 @ToString(callSuper = true)
 public abstract class StandardAuditable {
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     protected Instant createdAt;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     protected String createdBy;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     protected Instant updatedAt;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     protected String updatedBy;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     protected boolean deleted = false;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     protected Instant deletedAt;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     protected String deletedBy;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     protected Long version;
 }
