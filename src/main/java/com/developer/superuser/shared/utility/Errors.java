d281d8b2f749230161ba60c027a127ddadb0db3c
Kuntiarso <gilang.grk17@gmail.com>
2025-08-21 23:50:55 +0700

    feat(shared-library): updated errors util

--- src/main/java/com/developer/superuser/shared/utility/Errors.java
+++ src/main/java/com/developer/superuser/shared/utility/Errors.java
@@ -6,24 +6,40 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Errors {
     public ErrorData ok() {
+        return ok("Success");
+    }
+
+    public ErrorData ok(String message) {
         return ErrorData.builder()
                 .setResponseCode("200")
-                .setResponseMessage("OK")
+                .setResponseMessage(message)
                 .build();
     }
 
     public ErrorData badRequest() {
+        return badRequest("Bad request");
+    }
+
+    public ErrorData badRequest(String message) {
         return ErrorData.builder()
                 .setResponseCode("400")
-                .setResponseMessage("Bad Request")
+                .setResponseMessage(message)
                 .build();
     }
 
     public ErrorData internalServerError() {
+        return internalServerError("Internal server error");
+    }
+
+    public ErrorData internalServerError(String message) {
         return ErrorData.builder()
                 .setResponseCode("500")
-                .setResponseMessage("Internal Server Error")
+                .setResponseMessage(message)
                 .build();
+    }
+
+    public ErrorData error(String code) {
+        return error(code, "Unknown server error");
     }
 
     public ErrorData error(String code, String message) {
