661ca3a3b3d8a13b1545cde9e2c6d7e42b513a30
Kuntiarso <gilang.grk17@gmail.com>
2025-09-05 23:25:03 +0700

    feat(shared-library): added and modified some api schemas

---
+++ src/main/java/com/developer/superuser/shared/project/springodt/helper/Digest.java
@@ -0,0 +1,30 @@
+package com.developer.superuser.shared.project.springodt.helper;
+
+import com.developer.superuser.shared.helper.Generator;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+@RequiredArgsConstructor
+public class Digest implements Generator<String, String> {
+    public final String algorithm;
+
+    @Override
+    @SneakyThrows
+    public String generate(String request) {
+        MessageDigest md = MessageDigest.getInstance(algorithm);
+        md.update(request.getBytes(StandardCharsets.UTF_8));
+        return toHexLowerCase(md.digest());
+    }
+
+    private static String toHexLowerCase(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
